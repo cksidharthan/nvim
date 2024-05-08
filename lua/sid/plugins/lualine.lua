5a3d3ec4044f0a947cb6be2a23788441e86d13af
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-05-08 23:10:45 +0200

    add copilot status to lualine

--- lua/sid/plugins/lualine.lua
+++ lua/sid/plugins/lualine.lua
@@ -59,9 +59,30 @@ return {
         sections = {
           lualine_x = {
             {
-              lazy_status.updates,
-              cond = lazy_status.has_updates,
-              color = { fg = "#ff9e64" },
+              'copilot',
+              -- Default values
+              symbols = {
+                status = {
+                  icons = {
+                    enabled = " ",
+                    sleep = " ",   -- auto-trigger disabled
+                    disabled = " ",
+                    warning = " ",
+                    unknown = " "
+                  },
+                  hl = {
+                    enabled = "#50FA7B",
+                    sleep = "#AEB7D0",
+                    disabled = "#6272A4",
+                    warning = "#FFB86C",
+                    unknown = "#FF5555"
+                  }
+                },
+                spinners = require("copilot-lualine.spinners").dots,
+                spinner_color = "#6272A4"
+              },
+              show_colors = false,
+              show_loading = true
             },
             { "encoding" },
             { "fileformat", symbols = { unix = " " } },
