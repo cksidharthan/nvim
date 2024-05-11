1c19008924b49d5f848ea4e11b5a39938d18ffc9
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-05-12 01:00:54 +0200

    improve startup time

--- lua/sid/lazy.lua
+++ lua/sid/lazy.lua
@@ -22,12 +22,12 @@ require("lazy").setup({
   },
   change_detection = {
     -- automatically check for config file changes and reload the ui
-    enabled = true,
-    notify = true, -- get a notification when changes are found
+    enabled = false,
+    notify = false, -- get a notification when changes are found
   },
   checker = {
     enabled = true,
-    notify = true,
+    notify = false,
   },
   debug = false,
 })
