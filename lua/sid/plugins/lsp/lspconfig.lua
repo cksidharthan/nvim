244cb93278ed6cd3eafa1cf4cdee809e82697b96
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-05-31 23:02:28 +0200

    lazy loading config

--- lua/sid/plugins/lsp/lspconfig.lua
+++ lua/sid/plugins/lsp/lspconfig.lua
@@ -1,5 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
--- lua/sid/plugins/nvimtree.lua
+++ lua/sid/plugins/nvimtree.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = "nvim-tree/nvim-web-devicons",
+  event = "UIEnter",
 	config = function()
 		local nvimtree = require("nvim-tree")
 
