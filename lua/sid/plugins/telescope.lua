7a6d50e070bb8763a8571f3bbc297e689f64ad5b
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-06-06 15:49:16 +0200

    add some telescope keyamps, vimbegood and trouble setup

--- lua/sid/plugins/telescope.lua
+++ lua/sid/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+	tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -12,12 +12,13 @@ return {
 
 		telescope.setup({
 			defaults = {
+        prompt_prefix = " 🚀 ",
 				path_display = { "smart" },
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to previous result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 					},
 				},
 				file_ignore_patterns = { ".git/", "node_modules/", "vendor/" },
@@ -52,11 +53,13 @@ return {
 		-- find todos
 		keymap.set(
 			"n",
-			"<leader>ft",
-			"<cmd>Telescope todo<CR>",
+			"<leader>en",
+			"<cmd>Telescope find_files cwd=~/.config/nvim<CR>",
+      		{ desc = "Fuzzy find filetypes in Current working directory" },
 			{ desc = "Fuzzy find todos in Current working directory" }
 		)
 		keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Fuzzy find help tags" })
+		keymap.set("n", "<leader>fl", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Fuzzy find in current buffer" })
 		keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "Fuzzy find keymaps" })
 	end,
 }
