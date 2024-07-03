81d531c895117b975210228b16d816a0a10f287c
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-07-03 21:45:41 +0200

    fix

--- lua/sid/plugins/nvim-cmp.lua
+++ lua/sid/plugins/nvim-cmp.lua
@@ -41,10 +41,6 @@ return {
       },
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
       },
     })
   end,
