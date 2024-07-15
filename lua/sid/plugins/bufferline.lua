f1feaa99d717707aff3a698d238acba01020ec3c
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-07-15 17:08:04 +0200

    updates

--- lua/sid/plugins/bufferline.lua
+++ lua/sid/plugins/bufferline.lua
@@ -5,17 +5,17 @@ return {
 	config = function()
 		require("bufferline").setup({
 			options = {
-				mode = "buffer",
+				mode = "buffers",
 				show_tab_indicators = true,
         show_close_icon = true,
         color_icons = true,
         indicator = {
-          style = "round",
+          style = "icon",
         },
         buffer_close_icon = 'Ⅹ ',
         close_icon = 'Ⅹ ',
 				diagnostics = "nvim_lsp",
-        separator_style = "slope",
+        separator_style = "thick",
 				offsets = {
 					{
 						filetype = "NvimTree",
