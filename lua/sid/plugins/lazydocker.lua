23278f8645a21cd3faff00357408109dcec26088
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-08-06 20:28:57 +0200

    update lazydocker config to lazy load

--- lua/sid/plugins/lazydocker.lua
+++ lua/sid/plugins/lazydocker.lua
@@ -1,12 +1,15 @@
 return {
-  "crnvl96/lazydocker.nvim",
-  cmd = "LazyDocker",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("lazydocker").setup({})
-
-    vim.keymap.set({"n", "v"}, "<leader>ld", "<cmd>LazyDocker<CR>", { desc = "Toggle LazyDocker", noremap = true, silent = true })
-  end,
+	"crnvl96/lazydocker.nvim",
+	cmd = {
+		"LazyDocker",
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("lazydocker").setup({})
+	end,
+	keys = {
+		{ "<leader>ld", "<cmd>LazyDocker<cr>", desc = "Toggle LazyDocker" },
+	},
 }
