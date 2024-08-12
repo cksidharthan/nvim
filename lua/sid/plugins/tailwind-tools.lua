cfcf384fe06b8f34797ba2701e634b33a7fefa90
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-08-12 09:28:50 +0200

    add tailwind support

---
+++ lua/sid/plugins/tailwind-tools.lua
@@ -0,0 +1,16 @@
+-- tailwind-tools.lua
+return {
+	"luckasRanarison/tailwind-tools.nvim",
+	name = "tailwind-tools",
+	build = ":UpdateRemotePlugins",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-telescope/telescope.nvim", -- optional
+	},
+	opts = {}, -- your configuration
+	config = function()
+		require("tailwind-tools").setup({
+			-- your configuration
+		})
+	end,
+}
